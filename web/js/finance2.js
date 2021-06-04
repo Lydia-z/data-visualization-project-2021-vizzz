b8f1a2110ce77bb00abb08fd849e919ec9c41d82
YaoDI <y.di@outlook.com>
2021-06-04 03:07:24 +0200

    Finish the Finance part

--- web/js/finance2.js
+++ web/js/finance2.js
@@ -1,302 +1,3 @@
-var f2data1 = [
-  {
-    "title": "The Lion King",
-    "RunningTime": 118.0,
-    "Budget": 250000000.0,
-    "BoxOffice": 1657000000.0,
-    "ReleaseDate": "2019-07-09",
-    "imdb": 8.5,
-    "profit": 1407000000.0,
-    "ROI": 5.628,
-    "ProfitRatio": 6.628
-  }, {
-    "title": "Frozen II",
-    "RunningTime": 103.0,
-    "Budget": 150000000.0,
-    "BoxOffice": 1450000000.0,
-    "ReleaseDate": "2019-11-07",
-    "imdb": 6.9,
-    "profit": 1300000000.0,
-    "ROI": 8.6666666667,
-    "ProfitRatio": 9.6666666667
-  }, {
-    "title": "Frozen",
-    "RunningTime": 102.0,
-    "Budget": 150000000.0,
-    "BoxOffice": 1280000000.0,
-    "ReleaseDate": "2013-11-19",
-    "imdb": 7.4,
-    "profit": 1130000000.0,
-    "ROI": 7.5333333333,
-    "ProfitRatio": 8.5333333333
-  }, {
-    "title": "Beauty and the Beast",
-    "RunningTime": 129.0,
-    "Budget": 160000000.0,
-    "BoxOffice": 1264000000.0,
-    "ReleaseDate": "2017-02-23",
-    "imdb": 8.0,
-    "profit": 1104000000.0,
-    "ROI": 6.9,
-    "ProfitRatio": 7.9
-  }, {
-    "title": "Incredibles 2",
-    "RunningTime": 118.0,
-    "Budget": 200000000.0,
-    "BoxOffice": 1243000000.0,
-    "ReleaseDate": "2018-06-05",
-    "imdb": 7.6,
-    "profit": 1043000000.0,
-    "ROI": 5.215,
-    "ProfitRatio": 6.215
-  }, {
-    "title": "Zootopia",
-    "RunningTime": 108.0,
-    "Budget": 150000000.0,
-    "BoxOffice": 1024000000.0,
-    "ReleaseDate": "2016-02-13",
-    "imdb": 8.0,
-    "profit": 874000000.0,
-    "ROI": 5.8266666667,
-    "ProfitRatio": 6.8266666667
-  }, {
-    "title": "Toy Story 4",
-    "RunningTime": 100.0,
-    "Budget": 200000000.0,
-    "BoxOffice": 1073000000.0,
-    "ReleaseDate": "2019-06-11",
-    "imdb": 7.8,
-    "profit": 873000000.0,
-    "ROI": 4.365,
-    "ProfitRatio": 5.365
-  }, {
-    "title": "Toy Story 3",
-    "RunningTime": 103.0,
-    "Budget": 200000000.0,
-    "BoxOffice": 1067000000.0,
-    "ReleaseDate": "2010-06-12",
-    "imdb": 8.3,
-    "profit": 867000000.0,
-    "ROI": 4.335,
-    "ProfitRatio": 5.335
-  }, {
-    "title": "Finding Dory",
-    "RunningTime": 97.0,
-    "Budget": 175000000.0,
-    "BoxOffice": 1029000000.0,
-    "ReleaseDate": "2016-06-08",
-    "imdb": 7.3,
-    "profit": 854000000.0,
-    "ROI": 4.88,
-    "ProfitRatio": 5.88
-  }]; //profit
-
-var f2data2 = [
-  {
-    "title": "Bambi",
-    "RunningTime": 70.0,
-    "Budget": 858000.0,
-    "BoxOffice": 267400000.0,
-    "ReleaseDate": "1942-08-09",
-    "imdb": 7.3,
-    "profit": 266542000.0,
-    "ROI": 310.655011655,
-    "ProfitRatio": 311.655011655
-  }, {
-    "title": "The Jungle Book",
-    "RunningTime": 78.0,
-    "Budget": 4000000.0,
-    "BoxOffice": 378000000.0,
-    "ReleaseDate": "1967-10-18",
-    "imdb": 7.4,
-    "profit": 374000000.0,
-    "ROI": 93.5,
-    "ProfitRatio": 94.5
-  }, {
-    "title": "Cinderella",
-    "RunningTime": 74.0,
-    "Budget": 2900000.0,
-    "BoxOffice": 263600000.0000000298,
-    "ReleaseDate": "1950-02-15",
-    "imdb": 6.9,
-    "profit": 260700000.0000000298,
-    "ROI": 89.8965517241,
-    "ProfitRatio": 90.8965517241
-  }, {
-    "title": "One Hundred and One Dalmatians",
-    "RunningTime": 79.0,
-    "Budget": 3600000.0,
-    "BoxOffice": 303000000.0,
-    "ReleaseDate": "1961-01-25",
-    "imdb": 7.2,
-    "profit": 299400000.0,
-    "ROI": 83.1666666667,
-    "ProfitRatio": 84.1666666667
-  }, {
-    "title": "Pinocchio",
-    "RunningTime": 88.0,
-    "Budget": 2600000.0,
-    "BoxOffice": 164000000.0,
-    "ReleaseDate": "1940-02-07",
-    "imdb": 7.4,
-    "profit": 161400000.0,
-    "ROI": 62.0769230769,
-    "ProfitRatio": 63.0769230769
-  }, {
-    "title": "The Aristocats",
-    "RunningTime": 79.0,
-    "Budget": 4000000.0,
-    "BoxOffice": 191000000.0,
-    "ReleaseDate": "1970-12-11",
-    "imdb": 7.1,
-    "profit": 187000000.0,
-    "ROI": 46.75,
-    "ProfitRatio": 47.75
-  }, {
-    "title": "Lady and the Tramp",
-    "RunningTime": 76.0,
-    "Budget": 4000000.0,
-    "BoxOffice": 187000000.0,
-    "ReleaseDate": "1955-06-22",
-    "imdb": 7.3,
-    "profit": 183000000.0,
-    "ROI": 45.75,
-    "ProfitRatio": 46.75
-  }, {
-    "title": "Fantasia",
-    "RunningTime": 126.0,
-    "Budget": 2280000.0,
-    "BoxOffice": 83300000.0,
-    "ReleaseDate": "1940-11-13",
-    "imdb": 7.8,
-    "profit": 81020000.0,
-    "ROI": 35.5350877193,
-    "ProfitRatio": 36.5350877193
-  }, {
-    "title": "Song of the South",
-    "RunningTime": 94.0,
-    "Budget": 2125000.0,
-    "BoxOffice": 65000000.0,
-    "ReleaseDate": "1946-11-12",
-    "imdb": 7.1,
-    "profit": 62875000.0,
-    "ROI": 29.5882352941,
-    "ProfitRatio": 30.5882352941
-  }, {
-    "title": "Mary Poppins",
-    "RunningTime": 139.0,
-    "Budget": 4400000.0,
-    "BoxOffice": 103100000.0,
-    "ReleaseDate": "1964-08-27",
-    "imdb": 7.8,
-    "profit": 98700000.0,
-    "ROI": 22.4318181818,
-    "ProfitRatio": 23.4318181818
-  }]; //ROI
-
-var f2data3 = [
-  {
-    "title": "Mulan",
-    "RunningTime": 115.0,
-    "Budget": 200000000.0,
-    "BoxOffice": 57000000.0,
-    "ReleaseDate": "2020-03-09",
-    "imdb": 7.6,
-    "profit": -143000000.0,
-    "ROI": -0.715,
-    "ProfitRatio": 0.285
-  }, {
-    "title": "Mars Needs Moms",
-    "RunningTime": 88.0,
-    "Budget": 150000000.0,
-    "BoxOffice": 39200000.0,
-    "ReleaseDate": "2011-03-11",
-    "imdb": 5.4,
-    "profit": -110800000.0,
-    "ROI": -0.7386666667,
-    "ProfitRatio": 0.2613333333
-  }, {
-    "title": "Mighty Joe Young",
-    "RunningTime": 114.0,
-    "Budget": 90000000.0,
-    "BoxOffice": 50600000.0,
-    "ReleaseDate": "1998-12-25",
-    "imdb": 5.6,
-    "profit": -39400000.0,
-    "ROI": -0.4377777778,
-    "ProfitRatio": 0.5622222222
-  }, {
-    "title": "Onward",
-    "RunningTime": 102.0,
-    "Budget": 175000000.0,
-    "BoxOffice": 136400000.0,
-    "ReleaseDate": "2020-02-21",
-    "imdb": 7.4,
-    "profit": -38600000.0,
-    "ROI": -0.2205714286,
-    "ProfitRatio": 0.7794285714
-  }, {
-    "title": "Around the World in 80 Days",
-    "RunningTime": 120.0,
-    "Budget": 110000000.0,
-    "BoxOffice": 72200000.0,
-    "ReleaseDate": "2004-06-13",
-    "imdb": 5.9,
-    "profit": -37800000.0,
-    "ROI": -0.3436363636,
-    "ProfitRatio": 0.6563636364
-  }, {
-    "title": "Treasure Planet",
-    "RunningTime": 95.0,
-    "Budget": 140000000.0,
-    "BoxOffice": 109600000.0,
-    "ReleaseDate": "2002-11-27",
-    "imdb": 7.2,
-    "profit": -30400000.0,
-    "ROI": -0.2171428571,
-    "ProfitRatio": 0.7828571429
-  }, {
-    "title": "My Favorite Martian",
-    "RunningTime": 93.0,
-    "Budget": 65000000.0,
-    "BoxOffice": 36800000.0,
-    "ReleaseDate": "1999-02-12",
-    "imdb": 5.1,
-    "profit": -28200000.0,
-    "ROI": -0.4338461538,
-    "ProfitRatio": 0.5661538462
-  }, {
-    "title": "John Carter",
-    "RunningTime": 132.0,
-    "Budget": 306600000.0,
-    "BoxOffice": 284100000.0,
-    "ReleaseDate": "2012-02-22",
-    "imdb": 6.6,
-    "profit": -22500000.0,
-    "ROI": -0.0733855186,
-    "ProfitRatio": 0.9266144814
-  }, {
-    "title": "The Finest Hours",
-    "RunningTime": 117.0,
-    "Budget": 70000000.0,
-    "BoxOffice": 52100000.0,
-    "ReleaseDate": "2016-01-25",
-    "imdb": 6.8,
-    "profit": -17900000.0,
-    "ROI": -0.2557142857,
-    "ProfitRatio": 0.7442857143
-  }, {
-    "title": "The Country Bears",
-    "RunningTime": 88.0,
-    "Budget": 35000000.0,
-    "BoxOffice": 18000000.0,
-    "ReleaseDate": "2002-07-21",
-    "imdb": 4.1,
-    "profit": -17000000.0,
-    "ROI": -0.4857142857,
-    "ProfitRatio": 0.5142857143
-  }]; //loss
-
 var f2_slide = document.getElementById('f2_slide');
 
 var f2data_default = [{
@@ -315,10 +16,10 @@ var f2data_default = [{
 var f2legsvg = d3.select("#finance2-legend")
 
 // Handmade legend
-f2legsvg.append("circle").attr("cx",20).attr("cy",10).attr("r", 6).style("fill", "#613659")
-f2legsvg.append("circle").attr("cx",20).attr("cy",40).attr("r", 6).style("fill", "#D3B1C2")
-f2legsvg.append("text").attr("x", 40).attr("y", 10).text("Box Office").style("font-size", "15px").attr("alignment-baseline","middle")
-f2legsvg.append("text").attr("x", 40).attr("y", 40).text("Budget").style("font-size", "15px").attr("alignment-baseline","middle")
+f2legsvg.append("circle").attr("cx", 20).attr("cy", 10).attr("r", 6).style("fill", "#613659")
+f2legsvg.append("circle").attr("cx", 20).attr("cy", 40).attr("r", 6).style("fill", "#D3B1C2")
+f2legsvg.append("text").attr("x", 40).attr("y", 10).text("Box Office").style("font-size", "15px").attr("alignment-baseline", "middle")
+f2legsvg.append("text").attr("x", 40).attr("y", 40).text("Budget").style("font-size", "15px").attr("alignment-baseline", "middle")
 
 
 var f2margin = {
@@ -359,9 +60,23 @@ const f2yAxisGroup = f2svg.append("g")
   .attr("class", "y f2axis")
   .attr("transform", "translate(" + 0 + ",0)")
 
+var f2data1, f2data2, f2data3;
 
+d3.json("data/top10profit.json", d1 => {
+  f2data1 = d1
+})
+
+d3.json("data/top10ROI.json", d2 => {
+  f2data2 = d2
+})
+
+d3.json("data/top10loss.json", d3 => {
+  f2data3 = d3
+})
 var f2flagicon = 2
-f2update(f2data3)
+
+
+  
 
 function update0() {
   if (f2flagicon == 0) {
